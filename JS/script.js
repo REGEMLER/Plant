115ffc8dccdf5d2ad52506b368435b6fdbf8a858
Yar <yaroslav239@gmail.com>
2022-01-10 19:46:42 +0300

    corrected script

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,16 @@
-const btn1 = document.getElementById(`btn1`);
-const btn2 = document.getElementById(`btn2`);
-const text1 = document.getElementById(`btn-link1`);
-const text2 = document.getElementById(`btn-link2`);
 
-btn1.addEventListener(`mouseover`, ()=>{
-    text1.style.color = `white`;
-});
-btn2.addEventListener(`mouseover`, ()=>{
-    text2.style.color = `white`;
-});
-btn1.addEventListener(`mouseout`, ()=>{
-    text1.style.color = `#156963`;
-});
-btn2.addEventListener(`mouseout`, ()=>{
-    text2.style.color = `#156963`;
-});
- 
+const btn = [...document.getElementsByClassName(`header-btn`)];
+
+btn.forEach(item => {
+    item.addEventListener(`mouseover`,() => {
+        item.children[1].style.color = `white`;
+    } )
+})
+
+btn.forEach(item => {
+    item.addEventListener(`mouseout`,() => {
+        item.children[1].style.color = `#156963`;
+    } )
+})
+
+
